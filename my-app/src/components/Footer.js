079566404efca7d28520c20cc6de5b920c517afb
KoTe042 <arsidos3@gmail.com>
2024-05-17 01:57:04 +0300

    qwe

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>© 2024 Все права защищены.</p>
+        </footer>
+    );
+}
+
+export default Footer;
